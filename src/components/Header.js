8835efa0e0c0061fa8cbfad50ba3433dcd064c48
surfthecode <surfthecode@gmail.com>
2023-08-01 14:29:38 +0100

    create header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src=".images//meme-generator-logo.jpg" alt="meme generator logo" />
+      <h2>The meme generator</h2>
+    </header>
+  );
+};
+
+export default Header;
