49890fc3bb9162920a0bae8d2bcd43580ec75db4
surfthecode <surfthecode@gmail.com>
2023-08-01 15:52:04 +0100

    style header and main components

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -5,9 +5,12 @@ const Meme = () => {
   return (
     <main>
       <form>
-        <input type="text" className="from--input" placeholder="top text" />
-        <input type="text" className="from--input" placeholder="bottom text" />
-        <button className="form--btn">Get new meme</button>
+        <input type="text" className="form--input" placeholder="top text" />
+        <input type="text" className="form--input" placeholder="bottom text" />
+        <button className="form--btn">
+          Get new meme{" "}
+          <img src={trollface} alt="trollface" className="btn--logo" />
+        </button>
       </form>
     </main>
   );
