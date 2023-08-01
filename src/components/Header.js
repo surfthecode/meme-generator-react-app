63dbcc1bc520b67ff3ce4e5f23e7c70723a2e122
surfthecode <surfthecode@gmail.com>
2023-08-01 14:30:39 +0100

    add meme generator logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="header">
-      <img src=".images//meme-generator-logo.jpg" alt="meme generator logo" />
+      <img src="./images/logo.jpg" alt="meme generator logo" />
       <h2>The meme generator</h2>
     </header>
   );
