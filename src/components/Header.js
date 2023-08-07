93efc897a1704cc8f5f8519989a348f514a73ae6
surfthecode <surfthecode@gmail.com>
2023-08-07 12:08:17 +0100

    change app logo, free license; update app title styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
-import logo from "../assets/images/logo.jpg";
+import logo from "../assets/images/logo.png";
 
 const Header = () => {
   return (
     <header className="header">
       <nav>
         <div className="nav-left">
-          <img src={logo} alt="meme generator logo" className="logo" />
+          <img
+            src={logo}
+            alt="meme generator logo"
+            className="logo"
+            style={{ filter: "brightness(0.3) invert(1)" }}
+          />
           <h2>The meme generator</h2>
         </div>
       </nav>
