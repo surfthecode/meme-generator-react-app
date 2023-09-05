be69eb5803e6a43dbf1d30e64a15c0313263ba8b
surfthecode <surfthecode@gmail.com>
2023-09-05 13:54:39 +0300

    bugfix: logo url updated to live site

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,10 @@ const Header = () => {
   return (
     <header className="header">
       <nav>
-        <a href="/" className="logo-link">
+        <a
+          href="https://surfthecode.github.io/meme-generator-react-app/"
+          className="logo-link"
+        >
           <img
             src={logo}
             alt="meme generator logo"
